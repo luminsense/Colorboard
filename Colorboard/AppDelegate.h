cfa3a56e60e177e9cb24b946c3a7d7e2125a77ff
luminsense <wulianglumin@foxmail.com>
2014-08-05 01:07:09 +0800

    initial commit
    created storyboard

---
+++ Colorboard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Colorboard
+//
+//  Created by Lumi on 14-8-5.
+//  Copyright (c) 2014年 LumiNg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Colorboard/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Colorboard
+//
+//  Created by Lumi on 14-8-5.
+//  Copyright (c) 2014年 LumiNg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
